666d68a8a988fd8df0d6d622204de3b07e6d1860
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2023-07-17 11:02:11 +0200

    implement the login and setAvatar controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -26,3 +26,41 @@ module.exports.register = async (req, res, next) => {
     next(error);
   }
 };
+
+module.exports.login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.json({ msg: "Incorrect username or password", status: false });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.json({ msg: "Incorrect username or password", status: false });
+    }
+    delete user.password;
+    return res.json({ status: true, user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.setAvatar = async (req, res, next) => {
+  try {
+    const userId = req.params.id;
+    const avatarImage = req.body.image;
+    const userData = await User.findByIdAndUpdate(userId, {
+      isAvatarImageSet: true,
+      avatarImage,
+    });
+
+    return res.json({
+      isSet: userData.isAvatarImageSet,
+      image: userData.avatarImage,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
