a8429965db3ef7c60da5338e5eaeaf9684965c2c
Mahmoud-Elsamman <mahmoud.elsamman97@gmail.com>
2023-07-21 19:31:47 +0200

    create the Chat page logic and UI

--- public/src/pages/Chat.jsx
+++ public/src/pages/Chat.jsx
@@ -1,7 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { allUsersRoute } from "../utils/APIRoutes";
+import Contacts from "../components/Contacts";
+import axios from "axios";
 
-function chat() {
-  return <div>chat</div>;
+function Chat() {
+  const navigate = useNavigate();
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  useEffect(() => {
+    async function getCurrentUser() {
+      const user = localStorage.getItem("CHATMe-user");
+      if (!user) {
+        navigate("/login");
+      } else {
+        setCurrentUser(await JSON.parse(user));
+      }
+    }
+    getCurrentUser();
+  }, []);
+
+  useEffect(() => {
+    async function getAllUsers() {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    }
+    getAllUsers();
+  }, [currentUser]);
+
+  const handleChatChange = (chat) => {
+    setCurrentChat(chat);
+  };
+
+  return (
+    <Container>
+      <div className='container'>
+        <Contacts
+          contacts={contacts}
+          currentUser={currentUser}
+          changeChat={handleChatChange}
+        />
+      </div>
+    </Container>
+  );
 }
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 
-export default chat;
+export default Chat;
